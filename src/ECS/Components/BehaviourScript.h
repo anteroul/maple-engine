5cb626599df5d89c8df7416405b245380821137e
Antero Lindell <antero.lindell@gmail.com>
2024-02-01 11:01:26 +0200

    behaviour scripts

---
+++ src/ECS/Components/BehaviourScript.h
@@ -0,0 +1,22 @@
+#ifndef MAPLEENGINE_BEHAVIOURSCRIPT_H
+#define MAPLEENGINE_BEHAVIOURSCRIPT_H
+
+#include "../Component.h"
+#include <functional>
+
+class BehaviourScript : public Component {
+public:
+    BehaviourScript(Entity& owner, auto script) : Component(owner)
+    {
+        m_BehaviourScript = script;
+    }
+    void update(GLFWwindow* window, float deltaTime) override
+    {
+        m_BehaviourScript(deltaTime);
+    }
+private:
+    std::function<void(float)> m_BehaviourScript;
+};
+
+
+#endif //MAPLEENGINE_BEHAVIOURSCRIPT_H
---
+++ src/Scripts/TestScript.h
@@ -0,0 +1,11 @@
+#ifndef MAPLEENGINE_TESTSCRIPT_H
+#define MAPLEENGINE_TESTSCRIPT_H
+
+#include <iostream>
+
+static void test(float deltaTime)
+{
+    std::cout << "Delta Time: " << deltaTime << std::endl;
+}
+
+#endif //MAPLEENGINE_TESTSCRIPT_H
